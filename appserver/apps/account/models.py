a5647f40d0fcfc0cb0bb50722b8c5333ffd3e451
zip-sa <tmddn0505@gmail.com>
2025-12-24 18:56:12 +0900

    feat(models): update User model to use AwareDatetime for timestamps

--- appserver/apps/account/models.py
+++ appserver/apps/account/models.py
@@ -1,7 +1,9 @@
-from datetime import datetime
+from datetime import datetime, timezone
 from sqlmodel import SQLModel, Field
-from pydantic import EmailStr
+from pydantic import AwareDatetime, EmailStr
 from sqlalchemy import UniqueConstraint
+from sqlalchemy_utc import UtcDateTime
+from sqlalchemy.sql import func
 
 class User(SQLModel, table=True):
     __tablename__ = "users"
@@ -15,5 +17,20 @@ class User(SQLModel, table=True):
     display_name: str = Field(max_length=40, description="User Display Name")
     password: str = Field(max_length=128, description="User Password")
     is_host: bool = Field(default=False, description="Check Host")
-    created_at: datetime
-    updated_at: datetime
+    created_at: AwareDatetime = Field(
+        default=None,
+        nullable=False,
+        sa_type=UtcDateTime,
+        sa_column_kwargs={
+            "server_default": func.now(),
+        },
+    )
+    updated_at: AwareDatetime = Field(
+        default=None,
+        nullable=False,
+        sa_type=UtcDateTime,
+        sa_column_kwargs={
+            "server_default": func.now(),
+            "onupdate": lambda: datetime.now(timezone.utc),
+        },
+    )
