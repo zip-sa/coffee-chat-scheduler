628c2785a545e38aac41d5fdba7e677e18c6ff42
zip-sa <tmddn0505@gmail.com>
2025-12-25 21:01:53 +0900

    feat(models): update topics field to support PostgreSQL JSONB variant and add host relationship

--- appserver/apps/calendar/models.py
+++ appserver/apps/calendar/models.py
@@ -3,6 +3,8 @@ from typing import TYPE_CHECKING
 from pydantic import AwareDatetime
 from sqlalchemy_utc import UtcDateTime
 from sqlmodel import SQLModel, Field, Relationship, Text, JSON, func
+from sqlalchemy.dialects.postgresql import JSONB
+
 if TYPE_CHECKING:
     from appserver.apps.account.models import User
 
@@ -10,9 +12,12 @@ class Calendar(SQLModel, table=True):
     __tablename__ = "calendars" # type: ignore[arg-type]
 
     id: int = Field(default=None, primary_key=True)
-    topics: list[str] = Field(sa_type=JSON, default_factory=list, description="Topics for discussion with guests")
+    topics: list[str] = Field(sa_type=JSON().with_variant(JSONB(astext_type=Text()), "postgresql"), description="Topics for discussion with guests")
     google_calendar_id: str = Field(max_length=1024, description="Google Calendar ID")
 
+    host_id: int = Field(foreign_key="users.id", unique=True)
+    host: "User" = Relationship(back_populates="calendar")
+    
     created_at: AwareDatetime = Field(
         default=None,
         nullable=False,
