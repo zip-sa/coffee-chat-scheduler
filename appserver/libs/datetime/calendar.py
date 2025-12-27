0ae3ed79a010d074e6d9e58707111b0c7bc5005b
zip-sa <tmddn0505@gmail.com>
2025-12-27 09:59:35 +0900

    docs(calendar): enhance docstrings for get_start_weekday_of_month and get_last_day_of_month functions

--- appserver/libs/datetime/calendar.py
+++ appserver/libs/datetime/calendar.py
@@ -1,13 +1,31 @@
 from datetime import date, timedelta
 from tracemalloc import start
 
-# Get the starting weekday of a month (0=Monday, 6=Sunday)
-def get_start_weekday_of_month(year, month):
-    result = date(year, month, 1)
-    return result.weekday()
 
-# Get the last day of a month
+def get_start_weekday_of_month(year, month):
+    """
+    Get the starting weekday of a month (0=Monday, 6=Sunday)
+    
+    >>> get_start_weekday_of_month(2024, 12)
+    6
+    >>> get_start_weekday_of_month(2025, 2)
+    5
+    """
+    return date(year, month, 1).weekday()
+
 def get_last_day_of_month(year, month):
+    """
+    Get the last day of a month.
+
+    >>> get_last_day_of_month(2024, 2)
+    29
+    >>> get_last_day_of_month(2025, 2)
+    28
+    >>> get_last_day_of_month(2024, 4)
+    30
+    >>> get_last_day_of_month(2024, 12)
+    31
+    """
     if month == 12:
         next_month = date(year + 1, 1, 1)
     else:
@@ -17,6 +35,25 @@ def get_last_day_of_month(year, month):
     return result.day
 
 def get_range_days_of_month(year, month):
+    """
+    Get days of the month
+    
+    >>> result = get_range_days_of_month(2024, 3)
+    >>> result[:5]
+    [0, 0, 0, 0, 0]
+    >>> result[5]
+    1
+    >>> len(result)
+    36
+    >>> result = get_range_days_of_month(2024, 2) # Leap year
+    >>> result[:4]
+    [0, 0, 0, 0]
+    >>> result[4]
+    1
+    >>> len(result)
+    33
+    """
+
     # Get the starting weekday (0=Monday, 6=Sunday)
     start_weekday = get_start_weekday_of_month(year, month)
 
