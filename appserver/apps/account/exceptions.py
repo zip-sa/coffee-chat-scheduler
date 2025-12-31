21510c1cd8a7e53bc34c20856d22ae06c2d171f4
zip-sa <tmddn0505@gmail.com>
2025-12-31 20:08:57 +0900

    fix(exceptions): add DuplicatedEmailError for handling existing emails

--- appserver/apps/account/exceptions.py
+++ appserver/apps/account/exceptions.py
@@ -6,3 +6,10 @@ class DuplicatedUsernameError(HTTPException):
             status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
             detail="Username already exists.",
         )
+
+class DuplicatedEmailError(HTTPException):
+    def __init__(self):
+        super().__init__(
+            status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+            detail="Email already exists.",
+        )
