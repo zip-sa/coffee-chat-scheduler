ebdde4a65f38bf1dbe987f72a1cd322a15da8015
zip-sa <tmddn0505@gmail.com>
2026-01-10 00:26:34 +0900

    feat(calendar): register calendar router in main app
    Include calendar_router in FastAPI app to enable calendar endpoints.

--- appserver/app.py
+++ appserver/app.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from appserver.apps.account.endpoints import router as account_router
+from appserver.apps.calendar.endpoints import router as calendar_router
 
 app = FastAPI()
 
 def include_routers(_app: FastAPI):
     _app.include_router(account_router)
+    _app.include_router(calendar_router)
 
 include_routers(app)
